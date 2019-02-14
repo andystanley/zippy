fc2a8b586976f5a4f012b27a4ec64d6ce4af6e2b
Andrew Stanley <me@andrewstanley.ca>
2019-02-13 19:18:39 -0500

    Minor changes

--- cmd/zippy-cli.go
+++ cmd/zippy-cli.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -11,10 +12,12 @@ import (
 )
 
 func main() {
-	store.Open("../log")
+	path := flag.String("path", "./log", "path of the zippy store to open")
+	flag.Parse()
+	store.Open(*path)
+
 	fmt.Println("Welcome to zippy!")
 	scanner := bufio.NewScanner(os.Stdin)
-
 	for {
 		fmt.Print(">>> ")
 		scanner.Scan()
